609a519f55a5f83591c89ff3514a998664d4be5f
ATOM_MAX <56764208+ATOMMAX-2001@users.noreply.github.com>
2021-11-29 16:02:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
+//AUTHOR:S.ABILASH
 
 const express = require('express');
 const http = require('http');
 const {Server} = require('socket.io');
+const path = require('path');
+const datastore = require('nedb');
+const body = require('body-parser');
+
+const userdb = new datastore('user.db');
+//const usermessage = new datastore('message.db');
 
 const app = express();	
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.get('/',(req,res) =>{
-    res.sendFile(__dirname+'/index.html');
-});
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(body.urlencoded({extended:false}));
+app.use(body.json());
+userdb.loadDatabase();
+usermessage.loadDatabase();	
 
 let count =0;
 //credits:censor words given by Vishwa;
@@ -29,10 +39,45 @@ let censorWords=[
 	'kundi',
 	'oombu',
 	'dick',
-	'pool'
+	'pool',
+	'baadu',
+	'thevidiya'
 ];
+let users=[];
+
+app.get('/',(req,res) =>{
+	res.sendFile(__dirname+'/welcome.html');
+});
+
+app.get('/room',(req,res) =>{
+    res.sendFile(__dirname+'/index.html');    
+});
+let userName;
+app.post('/',(req,res)=>{
+	let sainame = req.body.username.trim();
+	userName = sainame;
+	userdb.find({name:userName},function(e,d){
+		if(d.length==1){
+			return res.redirect('/?user=notallowed');		
+		}else{
+			userdb.insert({name:sainame},function(e,d){
+				return res.redirect('/room?user='+sainame);
+			});
+		}
+	});
+})
+
+let userid;
 
 io.on("connection",(socket) =>{
+	if(users.indexOf(userName)>-1){
+		io.emit('intruder',socket.id);
+	}else{
+		userid = socket.id;
+		socket.id = userName;
+		users.push(userName);	
+	}
+	
 	count++;
 	socket.on('user-count',()=>{
 		io.emit('user-count',count);
@@ -52,14 +97,19 @@ io.on("connection",(socket) =>{
 					 }				
 				}
 			}
-			io.emit('students-message',tempMessage,count);
+			io.emit('students-message',tempMessage,socket.id,userid);
 		}
 	});
+function remove_user(username)
+{
+	userdb.remove({name:username},{});
+}
 	socket.on("disconnect",() => {
 		count--;
-		socket.on('user-counts',()=>{
-		io.emit('user-counts',count);
-		});
+		users.pop(socket.id);
+		remove_user(socket.id);
+		io.emit("user-leaving",count);
+		socket.emit('user-leaving');		
 	});
 
 });
